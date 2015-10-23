e42ab5fcff542edf243a884ebec03feb21ff5755
3rdderivative <ttkoch@noctrl.edu>
2015-10-23 00:42:09 -0500

    added in custom filter template for front end debugging. ~Not successful~ webpage spins

--- ttt_capstone_2015/graduate/forms.py
+++ ttt_capstone_2015/graduate/forms.py
@@ -3,6 +3,7 @@ Definition of forms.
 """
 
 from django import forms
+#from django.forms.models import model_to_dict
 from graduate.models import Race
 
 class PageOneForm(forms.Form): 
@@ -50,6 +51,8 @@ class PageOneForm(forms.Form):
     #    ('White','White'),
     #    )
     #race = forms.MultipleChoiceField(choices=RACE_CHOICES,widget=forms.CheckboxSelectMultiple())
+    #racedata = model_to_dict(Race.objects.all())
+    #race = forms.MultipleChoiceField(choices=racedata,widget=forms.CheckboxSelectMultiple())
     race = forms.ModelMultipleChoiceField(queryset=Race.objects.all(),widget=forms.CheckboxSelectMultiple())
     denomination = forms.ChoiceField(choices=(('db','Database'),('val','Values')))
     is_citizen = forms.ChoiceField(choices=(('yes','Yes'),('legal','Legal Permanent Resident'),('no','No')))
