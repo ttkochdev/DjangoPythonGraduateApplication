8ec75734996e03a33c8dd57b3b94e2bf53d9304d
3rdderivative <ttkoch@noctrl.edu>
2015-11-01 22:51:57 -0600

    login page and url patterns working. The HTML should follow NCC template before going live.

--- ttt_capstone_2015/graduate/forms.py
+++ ttt_capstone_2015/graduate/forms.py
@@ -112,17 +112,16 @@ class PageTwoForm(forms.Form):
     gi = forms.BooleanField()
 
 
-#from django.contrib.auth.forms import AuthenticationForm
-#from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
 
-#class BootstrapAuthenticationForm(AuthenticationForm):
-#    """Authentication form which uses boostrap CSS."""
-#    username = forms.CharField(max_length=254,
-#                               widget=forms.TextInput({
-#                                   'class': 'form-control',
-#                                   'placeholder': 'User name'}))
-#    password = forms.CharField(label=_("Password"),
-#                               widget=forms.PasswordInput({
-#                                   'class': 'form-control',
-#                                   'placeholder':'Password'}))
-#
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Password'}))
--- ttt_capstone_2015/ttk_capstone_2015/urls.py
+++ ttt_capstone_2015/ttk_capstone_2015/urls.py
@@ -4,7 +4,7 @@ Definition of urls for ttt_capstone_2015.
 
 from datetime import datetime
 from django.conf.urls import patterns, url
-#from graduate.forms import BootstrapAuthenticationForm
+from graduate.forms import BootstrapAuthenticationForm
 
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
@@ -20,24 +20,24 @@ urlpatterns = patterns('',
     url(r'^confirmation', 'graduate.views.confirmation', name='confirmation'), 
     #url(r'^contact$', 'graduate.views.contact', name='contact'),
     #url(r'^about', 'graduate.views.about', name='about'),
-    #url(r'^login/$',
-    #    'django.contrib.auth.views.login',
-    #    {
-    #        'template_name': 'graduate/login.html',
-    #        'authentication_form': BootstrapAuthenticationForm,
-    #        'extra_context':
-    #        {
-    #            'title':'Log in',
-    #            'year':datetime.now().year,
-    #        }
-    #    },
-    #    name='login'),
-    #url(r'^logout$',
-    #    'django.contrib.auth.views.logout',
-    #    {
-    #        'next_page': '/',
-    #    },
-    #    name='logout'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
